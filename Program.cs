1a3f619d4229fd67c397c003db7696d32d3a6503
Winston Leaphart <wleaphart@usf.edu>
2022-01-28 21:27:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Winston Leaphart
+using System;
+
+namespace Tech_assignment_2_Basic_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Andre 3000 is a top 3 rapper of all time - from the opinion of Winston Leaphart);
+        }
+    }
+}
